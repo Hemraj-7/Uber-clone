056980e2c3132a294f0bf809a7b6234b896fdaf5
Hemraj-7 <mhemraj814@gmail.com>
2025-01-10 15:30:03 +0530

    User authentication done

--- Backend/app.js
+++ Backend/app.js
@@ -4,11 +4,13 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const connectToDb = require('./db/db');
+const cookieParser = require("cookie-parser");
 const app = express();
 const userRoutes = require('./routes/user.routes')
 
 connectToDb();
 
+app.use(cookieParser())
 app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
